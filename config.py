f9fc8481a1e786642e4bd114f5ae6b4e5b9612ac
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-03-01 13:29:09 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,110 @@
+{
+    "application_id": "YOUR_APP_ID",
+    "timer_interval": 5,
+    "message_sets": [
+        {
+            "name": "TEXT SET 1",
+            "details": {
+                "text": "Future Playtest Q2 2024",
+                "enabled": true
+            },
+            "state": {
+                "text": "Closed Alpha",
+                "enabled": true
+            },
+            "large_image": {
+                "key": "https://i.imgur.com/Jz5brEb.gif",
+                "enabled": true
+            },
+            "small_image": {
+                "key": "https://i.imgur.com/L05svfQ.png",
+                "enabled": true
+            },
+            "large_text": {
+                "text": "Soon...",
+                "enabled": true
+            },
+            "small_text": {
+                "text": "Status: Server Offline",
+                "enabled": true
+            },
+            "buttons": [
+                {
+                    "label": "Join Now!",
+                    "url": "https://example.com",
+                    "enabled": true
+                },
+                {
+                    "label": "Watch Trailer",
+                    "url": "https://www.youtube.com/watch?v=wDG-DqlZBJ0",
+                    "enabled": true
+                }
+            ],
+            "party_size": {
+                "current_size": 11,
+                "max_size": 12,
+                "enabled": true
+            },
+            "startTimestamp": {
+                "time": 1635724800,
+                "enabled": true
+            },
+            "endTimestamp": {
+                "time": 1635732000,
+                "enabled": false
+            }
+        },
+        {
+            "name": "TEXT SET 2",
+            "details": {
+                "text": "Future Playtest Q2 2024",
+                "enabled": true
+            },
+            "state": {
+                "text": "Coming Soon",
+                "enabled": true
+            },
+            "large_image": {
+                "key": "https://i.imgur.com/Jz5brEb.gif",
+                "enabled": true
+            },
+            "small_image": {
+                "key": "https://i.imgur.com/L05svfQ.png",
+                "enabled": true
+            },
+            "large_text": {
+                "text": "Stay Tuned...",
+                "enabled": true
+            },
+            "small_text": {
+                "text": "Status: Server Offline",
+                "enabled": true
+            },
+            "buttons": [
+                {
+                    "label": "Join Now!",
+                    "url": "https://example.com",
+                    "enabled": true
+                },
+                {
+                    "label": "Watch Trailer!",
+                    "url": "https://www.youtube.com/watch?v=wDG-DqlZBJ0",
+                    "enabled": true
+                }
+            ],
+            "party_size": {
+                "current_size": 10,
+                "max_size": 12,
+                "enabled": true
+            },
+            "startTimestamp": {
+                "time": 1635724800,
+                "enabled": false
+            },
+            "endTimestamp": {
+                "time": 1635732000,
+                "enabled": false
+            }
+        }
+    ]
+}
