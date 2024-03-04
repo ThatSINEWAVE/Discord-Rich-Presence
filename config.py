3b19175edb60289a2a2546fa44922c38c1286a1f
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-03-04 04:42:03 +0200

    Updated config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 {
     "application_id": "YOUR_APP_ID",
-    "timer_interval": 5,
+    "timer_interval": 10,
     "message_sets": [
         {
-            "name": "TEXT SET 1",
+            "name": "PROJECT_CW",
             "details": {
-                "text": "Future Playtest Q2 2024",
+                "text": "Project Cold War",
                 "enabled": true
             },
             "state": {
@@ -31,59 +31,7 @@
             "buttons": [
                 {
                     "label": "Join Now!",
-                    "url": "https://example.com",
-                    "enabled": true
-                },
-                {
-                    "label": "Watch Trailer",
-                    "url": "https://www.youtube.com/watch?v=wDG-DqlZBJ0",
-                    "enabled": true
-                }
-            ],
-            "party_size": {
-                "current_size": 11,
-                "max_size": 12,
-                "enabled": true
-            },
-            "startTimestamp": {
-                "time": 1635724800,
-                "enabled": true
-            },
-            "endTimestamp": {
-                "time": 1635732000,
-                "enabled": false
-            }
-        },
-        {
-            "name": "TEXT SET 2",
-            "details": {
-                "text": "Future Playtest Q2 2024",
-                "enabled": true
-            },
-            "state": {
-                "text": "Coming Soon",
-                "enabled": true
-            },
-            "large_image": {
-                "key": "https://i.imgur.com/Jz5brEb.gif",
-                "enabled": true
-            },
-            "small_image": {
-                "key": "https://i.imgur.com/L05svfQ.png",
-                "enabled": true
-            },
-            "large_text": {
-                "text": "Stay Tuned...",
-                "enabled": true
-            },
-            "small_text": {
-                "text": "Status: Server Offline",
-                "enabled": true
-            },
-            "buttons": [
-                {
-                    "label": "Join Now!",
-                    "url": "https://example.com",
+                    "url": "https://projectcw.dev",
                     "enabled": true
                 },
                 {
@@ -93,9 +41,269 @@
                 }
             ],
             "party_size": {
-                "current_size": 10,
-                "max_size": 12,
+                "current_size": 100034,
+                "max_size": 100034,
                 "enabled": true
+            },
+            "startTimestamp": {
+                "time": 1635724800,
+                "enabled": false
+            },
+            "endTimestamp": {
+                "time": 1635732000,
+                "enabled": false
+            }
+        },
+        {
+            "name": "ERROR_404",
+            "details": {
+                "text": "<MISSING_TEXT_ENTRY>",
+                "enabled": true
+            },
+            "state": {
+                "text": "<STATE>",
+                "enabled": true
+            },
+            "large_image": {
+                "key": "https://i.imgur.com/hhZmhJy.png",
+                "enabled": true
+            },
+            "small_image": {
+                "key": "https://i.imgur.com/OnQXROj.png",
+                "enabled": true
+            },
+            "large_text": {
+                "text": "#IMAGE_NOT_FOUND",
+                "enabled": true
+            },
+            "small_text": {
+                "text": "#ICON_NOT_FOUND",
+                "enabled": true
+            },
+            "buttons": [
+                {
+                    "label": "#BUTTON_NOT_FOUND",
+                    "url": "https://www.google.com/404",
+                    "enabled": true
+                },
+                {
+                    "label": "<MISSING_STRING_TABLE_ENTRY>",
+                    "url": "https://www.google.com/404",
+                    "enabled": true
+                }
+            ],
+            "party_size": {
+                "current_size": 404,
+                "max_size": 404,
+                "enabled": true
+            },
+            "startTimestamp": {
+                "time": 1635724800,
+                "enabled": false
+            },
+            "endTimestamp": {
+                "time": 1635732000,
+                "enabled": false
+            }
+        },
+        {
+            "name": "BOT",
+            "details": {
+                "text": "Working with bots",
+                "enabled": true
+            },
+            "state": {
+                "text": "pycord = best",
+                "enabled": true
+            },
+            "large_image": {
+                "key": "https://i.imgur.com/e9LQVIT.png",
+                "enabled": true
+            },
+            "small_image": {
+                "key": "https://i.imgur.com/lqg1GPb.png",
+                "enabled": true
+            },
+            "large_text": {
+                "text": "Yes, i wrote this stuff",
+                "enabled": true
+            },
+            "small_text": {
+                "text": "Python 3.9",
+                "enabled": true
+            },
+            "buttons": [
+                {
+                    "label": "Python got me like",
+                    "url": "https://www.youtube.com/watch?v=vO5h-9Ibmck",
+                    "enabled": true
+                },
+                {
+                    "label": "GitHub ( Don't look pls )",
+                    "url": "https://github.com/ThatSINEWAVE",
+                    "enabled": true
+                }
+            ],
+            "party_size": {
+                "current_size": 5,
+                "max_size": 5,
+                "enabled": true
+            },
+            "startTimestamp": {
+                "time": 1635724800,
+                "enabled": false
+            },
+            "endTimestamp": {
+                "time": 1635732000,
+                "enabled": false
+            }
+        },
+        {
+            "name": "GLITCH",
+            "details": {
+                "text": "S\u00cc\u00b6T\u00cc\u00b7O\u00cc\u00b4P\u00cc\u00b6 \u00cc\u00b6W\u00cc\u00b7A\u00cc\u00b8T\u00cc\u00b5C\u00cc\u00b8H\u00cc\u00b6I\u00cc\u00b7N\u00cc\u00b4G\u00cc\u00b5",
+                "enabled": true
+            },
+            "state": {
+                "text": "I\u00cc\u00b6M\u00cc\u00b6 \u00cc\u00b7W\u00cc\u00b8A\u00cc\u00b4R\u00cc\u00b5N\u00cc\u00b6I\u00cc\u00b8N\u00cc\u00b6G\u00cc\u00b4 \u00cc\u00b4Y\u00cc\u00b4O\u00cc\u00b7U\u00cc\u00b5",
+                "enabled": true
+            },
+            "large_image": {
+                "key": "https://i.imgur.com/kUVOVgH.gif",
+                "enabled": true
+            },
+            "small_image": {
+                "key": "https://i.imgur.com/ikWCf2d.png",
+                "enabled": true
+            },
+            "large_text": {
+                "text": "W\u00cc\u00b5H\u00cc\u00b7Y\u00cc\u00b7 \u00cc\u00b5Y\u00cc\u00b4O\u00cc\u00b6U\u00cc\u00b7 \u00cc\u00b4L\u00cc\u00b6O\u00cc\u00b8O\u00cc\u00b7K\u00cc\u00b4I\u00cc\u00b5N\u00cc\u00b4G\u00cc\u00b8",
+                "enabled": true
+            },
+            "small_text": {
+                "text": "S\u00cc\u00b4T\u00cc\u00b4O\u00cc\u00b5P\u00cc\u00b7!\u00cc\u00b6!\u00cc\u00b5",
+                "enabled": true
+            },
+            "buttons": [
+                {
+                    "label": "D\u00cc\u00b7O\u00cc\u00b7N\u00cc\u00b7T\u00cc\u00b8 \u00cc\u00b4C\u00cc\u00b4L\u00cc\u00b5I\u00cc\u00b5C\u00cc\u00b6K\u00cc\u00b6",
+                    "url": "https://www.youtube.com/watch?v=Ym7dmZZzHwE",
+                    "enabled": true
+                },
+                {
+                    "label": "T\u00cc\u00b4E\u00cc\u00b6R\u00cc\u00b6M\u00cc\u00b6I\u00cc\u00b8N\u00cc\u00b6A\u00cc\u00b5L\u00cc\u00b7",
+                    "url": "https://geektyper.com/anon/",
+                    "enabled": true
+                }
+            ],
+            "party_size": {
+                "current_size": 22348586993838742234858699383874,
+                "max_size": 56749393738596722234858699383874,
+                "enabled": true
+            },
+            "startTimestamp": {
+                "time": 1635724800,
+                "enabled": false
+            },
+            "endTimestamp": {
+                "time": 1635732000,
+                "enabled": false
+            }
+        },
+        {
+            "name": "CRUNCHYCAT",
+            "details": {
+                "text": "why are you here?",
+                "enabled": true
+            },
+            "state": {
+                "text": "*blink*",
+                "enabled": true
+            },
+            "large_image": {
+                "key": "https://i.imgur.com/ZYG7W8V.gif",
+                "enabled": true
+            },
+            "small_image": {
+                "key": "https://i.imgur.com/tGUIToK.gif",
+                "enabled": true
+            },
+            "large_text": {
+                "text": "*blink*",
+                "enabled": true
+            },
+            "small_text": {
+                "text": "*crunch*",
+                "enabled": true
+            },
+            "buttons": [
+                {
+                    "label": "Delayed meow",
+                    "url": "https://www.youtube.com/watch?v=G5ICf1Ar2s4",
+                    "enabled": true
+                },
+                {
+                    "label": "Crunchy cat",
+                    "url": "https://www.youtube.com/watch?v=HMTKMWHLbdQ",
+                    "enabled": true
+                }
+            ],
+            "party_size": {
+                "current_size": 5,
+                "max_size": 5,
+                "enabled": false
+            },
+            "startTimestamp": {
+                "time": 1635724800,
+                "enabled": false
+            },
+            "endTimestamp": {
+                "time": 1635732000,
+                "enabled": false
+            }
+        },
+        {
+            "name": "WHY_THO",
+            "details": {
+                "text": "Why are you still here?",
+                "enabled": true
+            },
+            "state": {
+                "text": "fr now, why?",
+                "enabled": true
+            },
+            "large_image": {
+                "key": "https://i.imgur.com/iYs9CGJ.gif",
+                "enabled": true
+            },
+            "small_image": {
+                "key": "https://i.imgur.com/ik76xf2.png",
+                "enabled": true
+            },
+            "large_text": {
+                "text": "Thinking super hard",
+                "enabled": true
+            },
+            "small_text": {
+                "text": "Why you hover your mouse here?",
+                "enabled": true
+            },
+            "buttons": [
+                {
+                    "label": "Stop it, get some help",
+                    "url": "https://www.youtube.com/watch?v=l60MnDJklnM",
+                    "enabled": true
+                },
+                {
+                    "label": "BITCONEEEEEECT!",
+                    "url": "https://www.youtube.com/watch?v=e5nyQmaq4k4&ab_channel=DylanLocke",
+                    "enabled": true
+                }
+            ],
+            "party_size": {
+                "current_size": 404,
+                "max_size": 404,
+                "enabled": false
             },
             "startTimestamp": {
                 "time": 1635724800,
